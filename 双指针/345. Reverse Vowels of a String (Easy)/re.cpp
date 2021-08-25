ef62502f71c9331731616e77296d09da1a4bad5c
AozakiHayate <64193815+AozakiHayate@users.noreply.github.com>
2021-08-25 14:02:30 +0900

    Create re.cpp

---
+++ 双指针/345. Reverse Vowels of a String (Easy)/re.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        // 初始化双指针
+        int left = 0, right = s.length() - 1;
+        // 移动双指针
+        while(left < right) {
+            // 找到左边的元音
+            while(!judge(s[left]) && left < right) {
+                left ++;
+            }
+            // 找到右边的元音
+            while(!judge(s[right]) && left < right) {
+                right --;
+            }
+            // 调换
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left ++;
+            right --;
+        }
+        return s;
+    }
+    // 判断是否是元音
+    bool judge(char a) {
+        if(a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' || a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U') {
+            return true;
+        }
+        return false;
+    }
+};
