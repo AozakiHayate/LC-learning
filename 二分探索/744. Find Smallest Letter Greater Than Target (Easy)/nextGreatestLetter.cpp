4b2e30ea0dcc9c82b1df3445e73acb040e9d38aa
AozakiHayate <64193815+AozakiHayate@users.noreply.github.com>
2021-09-19 14:43:43 +0900

    Create nextGreatestLetter.cpp

---
+++ 二分探索/744. Find Smallest Letter Greater Than Target (Easy)/nextGreatestLetter.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int left=0,right=letters.size()-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(letters[mid]>target)right=mid;
+            else left=mid+1;
+        }
+        if(letters[left]<=target)return letters[0];
+        return letters[left];
+    }
+};
