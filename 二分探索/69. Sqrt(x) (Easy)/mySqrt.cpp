1a591089caf273489dc9cd61a454a081d4768b12
AozakiHayate <64193815+AozakiHayate@users.noreply.github.com>
2021-10-04 20:12:35 +0900

    Update mySqrt.cpp

--- 二分探索/69. Sqrt(x) (Easy)/mySqrt.cpp
+++ 二分探索/69. Sqrt(x) (Easy)/mySqrt.cpp
@@ -4,7 +4,7 @@ public:
         int l = 0, r = x, ans = -1;
         while (l <= r) {
             int mid = l + (r - l) / 2;
-            if ((long long)mid * mid <= x) {
+            if ((lon long)mid * mid <= x) {
                 ans = mid;
                 l = mid + 1;
             } else {
