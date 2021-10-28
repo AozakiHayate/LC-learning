4118f257c61660ce33ff24c115649762a37aed38
AozakiHayate <64193815+AozakiHayate@users.noreply.github.com>
2021-10-28 17:14:24 +0900

    Update integerBreak1.cpp

--- DP動的計画法/343. Integer Break (Medim)/integerBreak1.cpp
+++ DP動的計画法/343. Integer Break (Medim)/integerBreak1.cpp
@@ -4,6 +4,7 @@ public:
         vector <int> dp(n + 1);
         for (int i = 2; i <= n; i++) {
             int curMax = 0;
+            
             for (int j = 1; j < i; j++) {
                 curMax = max(curMax, max(j * (i - j), j * dp[i - j]));
             }
