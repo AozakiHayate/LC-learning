dd70fad396382f4362b2ec131bc9b77efc0c76f0
AozakiHayate <64193815+AozakiHayate@users.noreply.github.com>
2021-10-29 17:06:50 +0900

    Create minMoves2.java

---
+++ 数学/462. Minimum Moves to Equal Array Elements II (Medium)/minMoves2.java
@@ -0,0 +1,11 @@
+public int minMoves2(int[] nums) {
+    Arrays.sort(nums);
+    int move = 0;
+    int l = 0, h = nums.length - 1;
+    while (l <= h) {
+        move += nums[h] - nums[l];
+        l++;
+        h--;
+    }
+    return move;
+}
