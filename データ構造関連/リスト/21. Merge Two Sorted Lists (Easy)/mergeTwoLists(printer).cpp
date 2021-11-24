2ae5636f7dfc2b7a7ce4d0ab1b934eaa46b18ec4
AozakiHayate <64193815+AozakiHayate@users.noreply.github.com>
2021-11-24 10:30:45 +0900

    Update mergeTwoLists(printer).cpp

--- データ構造関連/リスト/21. Merge Two Sorted Lists (Easy)/mergeTwoLists(printer).cpp
+++ データ構造関連/リスト/21. Merge Two Sorted Lists (Easy)/mergeTwoLists(printer).cpp
@@ -14,8 +14,6 @@ public:
             }
             prev = prev->next;
         }
-
-        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
         prev->next = l1 == nullptr ? l2 : l1;
 
         return preHead->next;
