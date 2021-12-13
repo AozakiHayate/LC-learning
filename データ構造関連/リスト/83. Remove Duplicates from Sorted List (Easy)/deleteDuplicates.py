734af4adb29dcbd15a3234f46e11f440f3f1b3ad
AozakiHayate <64193815+AozakiHayate@users.noreply.github.com>
2021-12-13 12:27:32 +0900

    Update deleteDuplicates.py

--- データ構造関連/リスト/83. Remove Duplicates from Sorted List (Easy)/deleteDuplicates.py
+++ データ構造関連/リスト/83. Remove Duplicates from Sorted List (Easy)/deleteDuplicates.py
@@ -2,7 +2,6 @@ class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
         if not head:
             return head
-
         cur = head
         while cur.next:
             if cur.val == cur.next.val:
