2d64f1eb0b6adb41ebf81672feeae2eac8eb2cb1
AozakiHayate <64193815+AozakiHayate@users.noreply.github.com>
2021-12-14 09:56:04 +0900

    Update reverseList.cpp

--- データ構造関連/リスト/206. Reverse Linked List (Easy)/reverseList.cpp
+++ データ構造関連/リスト/206. Reverse Linked List (Easy)/reverseList.cpp
@@ -4,7 +4,7 @@ public:
         if (!head || !head->next) {
             return head;
         }
-        
+    
         ListNode* newHead = reverseList(head->next);
         head->next->next = head;
         head->next = nullptr;
